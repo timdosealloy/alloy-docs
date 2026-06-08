ad021e5af78f4b596d1bf4aa763fb05eb4d7eacc
Tim Dosé <tim.dose@alloy.com>
2026-06-08 17:08:09 -0400

    trying astro-relative-links plugin to make installation work in subfolder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import relativeLinks from 'astro-relative-links';
+
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), relativeLinks()],
   site: 'https://timdosealloy.github.io',
   base: 'alloy-docs',
 });
